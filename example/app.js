b188b65f0750ff9544de735e44ab5232f81eb160
Didier Colens <dcolens@cisco.com>
2013-04-19 14:10:15 +0200

    major rewrite

--- example/app.js
+++ example/app.js
@@ -1,32 +1,33 @@
 #!/usr/bin/env node
 
-var connect = require('connect'),
-    FileSessionStore = require('../lib/connect-session-folder');
+var express = require('express')
+  , FMStore = require('./')(express)
+  , app = express()
+  ;
 
-connect(
-      connect.cookieParser()
-    , connect.session({ 
-          secret:'session file'
-        , store: new FileSessionStore({path:'.', printDebug:true, useAsync:true})
-    })
-    , connect.favicon()
-    , function(req,res,next) {
-        var sess = req.session;
-        console.log('+ begin ' + req.url );
-        if (sess.views) {
-            sess.views++;
-            res.setHeader('Content-Type', 'text/html');
-            res.write('<p>views: ' + sess.views + '</p>');
-            res.write('<p>expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>');
-            res.write('<img src="blank.png"/>');
-            res.write('<img src="blank2.png"/>');
-            res.write('<img src="blank3.png"/>');
-            res.write('<img src="blank4.png"/>');
-            res.write('<img src="blank5.png"/>');
-            res.end();
-        } else {
-            sess.views = 1;
-            res.end('welcome to the file session demo. refresh!');
-        }
-    }
-).listen(8080);
+var myStore = new FMStore({parentFolder: __dirname+'/mySessions', permission: '0755'});
+
+//Sample middleware to create the folder and provide the folder name in req.session.folder
+function setFolder(req, res, next) {
+    logger.trace('setFolder', req.sessionID);
+    mySessionStore.checkP({sid: req.sessionID, user: req.user}).then(function(dirName) {
+        req.session.folder = dirName;
+        next(); 
+    }, function(e) { res.json(500, e); });
+};
+
+app.use(express.cookieParser());
+app.use(express.session({
+    cookie: conf.sessions.cookie
+    , secret: conf.sessions.secret
+    , store: mySessionStore
+}));
+
+app.use(app.router);
+
+app.get('/mySessionFolder', setFolder, function(req, res) {
+    // req.session.folder should point to this user's session folder
+    res.send(req.session.folder);
+} );
+
+app.listen(8080);
---
+++ test.js
@@ -0,0 +1,64 @@
+
+/*jshint smarttabs:true, evil:true, undef:true, node:true, wsh:true, eqnull:true, expr:true, curly:true, trailing:false , unused:true, laxcomma:true*/
+'use strict';
+/**
+ * Module dependencies.
+ */
+
+var assert = require('assert')
+  , connect = require('connect')
+  , FMStore = require('./')(connect)
+  , fs = require('fs')
+  , parentFolder = __dirname+'/testStore'
+  , permission = '0755';
+  ;
+
+assert.ok(!fs.existsSync(parentFolder), parentFolder+ ' should not exist');
+
+var store = new FMStore({parentFolder: parentFolder, permission: permission});
+
+store.on('connect', function(){
+
+  assert.ok(fs.existsSync(parentFolder), 'parentFolder not created');
+  var stat = fs.statSync(parentFolder);
+  assert.equal(parseInt(stat.mode.toString(8), 10), '4'+permission, 'permission should be '+permission+' for '+parentFolder);
+
+  // #set()
+  store.set('123', { cookie: { maxAge: 2000 }, name: 'dc' }, function(err){
+    assert.ok(!err, '#set() got an error');
+
+    store.checkP({sid: '123', otherStuff:'lalala'}).then(function(folder) {
+
+      assert.ok(!!folder, 'folder name not returned');
+      assert.ok(fs.existsSync(parentFolder+'/'+folder), 'session folder not created');
+      assert.ok(fs.existsSync(parentFolder+'/'+folder+'/session-info'), 'session-info file not created');
+
+      // #get()
+      store.get('123', function(err, data){
+
+        assert.ok(!err, '#get() got an error');
+
+        assert.deepEqual({ cookie: { maxAge: 2000 }, name: 'dc' }, data);
+
+        // #set null
+        store.set('123', { cookie: { maxAge: 2000 }, name: 'dc' }, function(){
+          store.destroy('123', function(e){
+            assert.ok(!e, "destory should succeed");
+            fs.rmdirSync(parentFolder);
+            assert.ok(!fs.existsSync(parentFolder), parentFolder+ " should not exist");
+            store.length(function(e,l) {
+              assert.equal(l, 0, "store should be empty");
+              console.log('done');
+              process.exit();
+            });
+          });
+        });
+        throw new Error('Error in fn');
+      });      
+    }).done();
+  });
+});
+
+process.once('uncaughtException', function (err) {
+  assert.ok(err.message === 'Error in fn', '#get() catch wrong error');
+});
